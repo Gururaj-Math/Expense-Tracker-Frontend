31657e678858d486cdfe12c389f8b05873d6ddba
Gururaj-Math <gururajmath3@gmail.com>
2023-10-14 16:09:37 +0530

    initialize

---
+++ exp-tracker/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import React from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
