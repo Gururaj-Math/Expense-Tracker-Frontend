f2207bbebc90d6f429d86b04be634fb6a97be9a8
Gururaj-Math <gururajmath3@gmail.com>
2023-10-14 16:35:03 +0530

    tailwind config update

--- exp-tracker/src/App.tsx
+++ exp-tracker/src/App.tsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
 import './App.css'
 import React from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <h1>Expense Tracker</h1>
     </>
   )
 }
