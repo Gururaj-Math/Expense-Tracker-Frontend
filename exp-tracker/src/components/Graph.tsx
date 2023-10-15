624e69442a35fd0fdd72fe7674452944b0134f37
Gururaj-Math <gururajmath3@gmail.com>
2023-10-15 12:00:00 +0530

    graph update

--- exp-tracker/src/components/Graph.tsx
+++ exp-tracker/src/components/Graph.tsx
@@ -4,28 +4,36 @@ import { Doughnut } from "react-chartjs-2";
 
 Chart.register(ArcElement);
 
-const data = {
-  labels: ["Red", "Blue", "Yellow"],
-  datasets: [
-    {
-      label: "My First Dataset",
-      data: [300, 50, 100],
-      backgroundColor: [
-        "rgb(255, 99, 132)",
-        "rgb(54, 162, 235)",
-        "rgb(255, 205, 86)",
-      ],
-      hoverOffset: 4,
-    },
-  ],
-};
+const config = {
+  data:{
+    datasets: [
+      {
+        data: [300, 50, 100],
+        backgroundColor: [
+          "rgb(255, 99, 132)",
+          "rgb(54, 162, 235)",
+          "rgb(255, 205, 86)",
+        ],
+        hoverOffset: 4,
+        borderRadius: 30,
+        spacing:10
+      },
+    ],
+  },
+  options:{
+    cutout: 115
+  }
+}
 
 const Graph = () => {
   return (
     <div className="flex justify-content max-w-xs mx-auto">
       <div className="item">
         <div className="chart relative">
-          <Doughnut data={data} />
+          <Doughnut {...config}/>
+          <h3 className="mb-4 font-bold absolute left-0 right-0 top-[40%] ml-auto mr-auto">Total
+          <span className="block text-3xl text-emerald-400">${0}</span>
+          </h3>
         </div>
         <div className="flex flex-col py-10 gap-4">{/* labels */}</div>
       </div>
