1369b9acd9aa03f6b86804b3b41bbbd25f158b61
Gururaj-Math <gururajmath3@gmail.com>
2023-10-15 12:40:54 +0530

    react hook form add

--- exp-tracker/src/components/Form.tsx
+++ exp-tracker/src/components/Form.tsx
@@ -1,31 +1,47 @@
 import React from "react";
-
+import { useForm } from "react-hook-form";
 
 export default function Form() {
+  const { register, handleSubmit, resetField } = useForm();
+
+  const onSubmit = (data) => {
+    console.log("data", data);
+  };
+
   return (
     <div className="max-w-sm mx-auto w-96">
       <h1 className="font-bol pb-4 text-xl">Transaction</h1>
-      <form id="form">
+      <form id="form" onSubmit={handleSubmit(onSubmit)}>
         <div className="grid gap-4">
           <div className="input-group">
             <input
               type="text"
               placeholder="Salary, Rent, SIP"
               className="form-input"
+              {...register("name")}
             />
           </div>
-          <select className="form-input">
-            <option value="Investment" defaultValue="Investment">
-              Investment
-            </option>
+          <select
+            className="form-input"
+            {...register("type")}
+            defaultValue="Investment"
+          >
+            <option value="Investment">Investment</option>
             <option value="Expence">Expence</option>
             <option value="Savings">Savings</option>
           </select>
           <div className="input-group">
-            <input type="text" placeholder="Amount" className="form-input" />
+            <input
+              type="text"
+              placeholder="Amount"
+              className="form-input"
+              {...register("amount")}
+            />
           </div>
           <div>
-            <button className="border py-2 text-white bg-indigo-500 w-full">Add Transaction</button>
+            <button className="border py-2 text-white bg-indigo-500 w-full">
+              Add Transaction
+            </button>
           </div>
         </div>
       </form>
